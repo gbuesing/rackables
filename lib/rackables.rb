06c008be4faff4dc0e30649116fab7ccda91ad27
Geoff Buesing <geoff@lifesnapz.com>
2010-01-12 18:25:19 -0600

    Add PublicExceptionPage

--- lib/rackables.rb
+++ lib/rackables.rb
@@ -1,4 +1,5 @@
 module Rackables
   autoload :DefaultCharset, 'rackables/default_charset'
+  autoload :PublicExceptionPage, 'rackables/public_exception_page'
   autoload :TrailingSlashRedirect, 'rackables/trailing_slash_redirect'
 end
