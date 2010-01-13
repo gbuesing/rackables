3690bca83f3f5518b25c1c1426419896de29457a
Geoff Buesing <geoff@lifesnapz.com>
2010-01-13 17:11:58 -0600

    Add Rackables::Branch

--- lib/rackables.rb
+++ lib/rackables.rb
@@ -1,6 +1,7 @@
 module Rackables
-  autoload :CacheControl, 'rackables/cache_control'
-  autoload :DefaultCharset, 'rackables/default_charset'
-  autoload :PublicExceptionPage, 'rackables/public_exception_page'
-  autoload :TrailingSlashRedirect, 'rackables/trailing_slash_redirect'
+  autoload :Branch,                 'rackables/branch'
+  autoload :CacheControl,           'rackables/cache_control'
+  autoload :DefaultCharset,         'rackables/default_charset'
+  autoload :PublicExceptionPage,    'rackables/public_exception_page'
+  autoload :TrailingSlashRedirect,  'rackables/trailing_slash_redirect'
 end
---
+++ test/test_branch.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require 'rackables'
+
+class TestBranch < Test::Unit::TestCase
+
+  def test_calls_app_returned_from_block
+    app = Proc.new { [200, {}, ["Downstream app"]] }
+    blog = Proc.new { [200, {}, ["Blog"]] }
+    middleware = Rackables::Branch.new(app) do |env|
+      blog if env['PATH_INFO'] =~ /^\/blog/
+    end
+    status, headers, body = middleware.call('PATH_INFO' => '/blog')
+    assert_equal 'Blog', body[0]
+  end
+
+  def test_calls_app_passed_in_to_initialize_when_block_returns_false
+    app = Proc.new { [200, {}, ["Downstream app"]] }
+    blog = Proc.new { [200, {}, ["Blog"]] }
+    middleware = Rackables::Branch.new(app) do |env|
+      blog if env['PATH_INFO'] =~ /^\/blog/
+    end
+    status, headers, body = middleware.call('PATH_INFO' => '/foo')
+    assert_equal 'Downstream app', body[0]
+  end
+
+end
